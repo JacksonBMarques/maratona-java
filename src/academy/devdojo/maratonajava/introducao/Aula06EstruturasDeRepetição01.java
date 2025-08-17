00654c39218f6e15c1b64044203e1f071b32ebd2
Jackson Marques <m.jbrenodev@outlook.com>
2025-08-17 16:44:25 -0300

    Estruturas de repetição aula 01

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepetição01 {
+    public static void main(String[] args) {
+        // While, do-while, for
+        int count =0;
+        while (count < 10){
+            System.out.println(count);
+            count++;
+
+        }
+        count = 0;
+        do {
+            System.out.println("dentro do do-while " + count);
+            count++;
+        }while (count < 10);
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("dentro do for " + i);
+        }
+
+    }
+}
