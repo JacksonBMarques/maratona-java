839590e0203375556bdbc33c19173800d16fe09a
Jackson Marques <m.jbrenodev@outlook.com>
2025-08-17 17:21:00 -0300

    Estruturas de repetição aula 04

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao04 {
+    // Dado o valor de um carro descubra em quantas vezes ele pode ser parcelado
+    // Condição valor da parcela >= 10000
+    public static void main(String[] args) {
+        double valorCarro = 30000;
+
+
+        for (int parcela = 1; parcela <= valorCarro ; parcela++) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela >= 1000){
+               System.out.println("Parcela "+ parcela+" R$ "+ valorParcela);
+            }
+            else{
+                break;
+            }
+            }
+        System.out.println(valorCarro);
+        }
+
+
+    }
+
+
