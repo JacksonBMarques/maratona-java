aae7a3c327f4d00dc165278d3a02128876aad209
Jackson Marques <m.jbrenodev@outlook.com>
2025-08-25 11:22:00 -0300

    Aula07Arrays01 - Declaração é definição de um array

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -2,6 +2,9 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays01 {
     public static void main(String[] args) {
+        // Um array é uma estrutura de dados que define uma referencia a um espaço de memória
+        // deve ser usado para facilitar a criação de listas, no exemplo abaixo temos um arrat listando idades
+        // na pratica as tres idades declaradas ocupam o mesmo local na memoria diferenciados pelo indice [1]
         int[] idades = new int[3];
         idades[0] = 21;
         idades[1] = 15;
