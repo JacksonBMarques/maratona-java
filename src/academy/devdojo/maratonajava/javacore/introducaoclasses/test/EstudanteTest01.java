7442f4caa87091f6e78173a61b124f209edcd91f
Jackson Marques <m.jbrenodev@outlook.com>
2025-08-25 16:02:21 -0300

    POO introdução

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 18;
+        estudante.sexo = 'M';
+
+        Estudante sasuke = new Estudante();
+        sasuke.nome = "naruto";
+        sasuke.idade = 18;
+        sasuke.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println("==================");
+        System.out.println(sasuke.nome);
+        System.out.println(sasuke.idade);
+        System.out.println(sasuke.sexo);
+
+    }
+}
