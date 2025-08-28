e3322d9540717b3bc04b366622fe7f61f69e313e
Jackson Marques <m.jbrenodev@outlook.com>
2025-08-28 11:52:34 -0300

    Metodos com passagem de parametros de refernecia pt03 - RFERENCIA THIS

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+
+        estudante01.nome = "midoriya";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Sakura";
+        estudante02.idade = 15;
+        estudante02.sexo = 'F';
+
+        estudante01.imprime();
+        System.out.println("----------------------");
+        estudante02.imprime();
+    }
+}
