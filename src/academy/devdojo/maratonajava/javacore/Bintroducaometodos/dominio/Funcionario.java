cdd3d3b55621cae2ce9bc389e50658f29f150d56
Jackson Marques <m.jbrenodev@outlook.com>
2025-08-28 15:32:10 -0300

    Modificadores de acesso Private, get, set

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -4,22 +4,24 @@ public class Funcionario {
     public String nome;
     public int idade;
     public double[] salario;
-pligoin
+
+
     public void imprime (){
         System.out.println(this.nome);
         System.out.println(this.idade);
-        for (int i = 0; i < salario.length ; i++) {
-            System.out.println("salario " + i + ": "+ salario[i]);
-
+        if (salario == null){
+            return;
+        }else {
+            for (int i = 0; i < salario.length ; i++) {
+                 System.out.println("salario " + i + ": " + salario[i]);
+            }
         }
     }
 
     public void mediaSalario(){
         double media = 0;
-        for (double salario: salario) {
-            media += salario;
-
-
+        for (int i =0; i< salario.length; i++) {
+            media = media + salario[i];
         }
         media = media / salario.length;
         System.out.println(media);
