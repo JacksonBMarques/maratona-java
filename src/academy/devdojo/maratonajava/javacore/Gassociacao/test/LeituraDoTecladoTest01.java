36bb0b354c2a063de7a2fed96331850011de651c
Jackson Marques <m.jbrenodev@outlook.com>
2025-09-04 21:10:21 -0300

    scanner

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/LeituraDoTecladoTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String next = input.nextLine();
+        System.out.println("--------------");
+
+        String next2 = input.nextLine();
+        System.out.println(next);
+        System.out.println(next2);
+    }
+}
